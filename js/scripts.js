84347f40aa8b4185c9d8e4408f61691c036e07a8
postolenko <denis.postolenko@gmail.com>
2020-11-02 23:01:09 +0200

    QWERTY

--- js/scripts.js
+++ js/scripts.js
@@ -25,33 +25,42 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
-            dots: true,
-            arrows: true,
-            autoplay: true,
+    if( $(".testimonial_slider").length > 0 ) {
+        $(".testimonial_slider").not(".slick-initialized").slick({
+            dots: false,
+            arrows: false,
+            // autoplay: true,
             autoplaySpeed: 4000,
             speed: 1200,
             slidesToShow: 4,
             slidesToScroll: 1,
             // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
         });
     }
+
+    $( ".thumb_2" ).bind({
+      mouseenter: function() {
+        $( this ).find(".thumb_2_text").slideDown(300);
+      },
+      mouseleave: function() {
+        $( this ).find(".thumb_2_text").slideUp(300);
+      }
+    });
 
 });
