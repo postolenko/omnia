2bf99a75a5185785773ae407d2ee8cc97dfb2a7a
postolenko <denis.postolenko@gmail.com>
2020-11-13 19:01:14 +0200

    QWERTY

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,8 @@ function getRightCoord() {
 }
 
 function getMeetUsSlider() {
+  if($(".thumbnails_2").length > 0) {
+    
     $(".thumbnails_2").not(".slick-initialized").slick({
       speed: 1200,
       slidesToShow: 1,
@@ -34,6 +36,19 @@ function getMeetUsSlider() {
           }
       ]
     });
+
+    $( ".thumb_2" ).bind({
+      mouseenter: function() {
+        $( this ).find(".thumb_2_text").slideDown(300);
+        console.log("mouseenter");
+      },
+      mouseleave: function() {
+        $( this ).find(".thumb_2_text").slideUp(300);
+        console.log("mouseleave");
+      }
+    });
+
+  }
 }
 
 function getPricesSlider() {
@@ -61,155 +76,7 @@ function getPricesSlider() {
     });
 }
 
-
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-
-$(window).load(function() {
-  getRightCoord();
-});
-
-$(window).resize(function() {
-  getRightCoord();
-  bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-  getMeetUsSlider();
-  getPricesSlider();
-  $( ".thumb_2" ).bind({
-    mouseenter: function() {
-      $( this ).find(".thumb_2_text").slideDown(300);
-    },
-    mouseleave: function() {
-      $( this ).find(".thumb_2_text").slideUp(300);
-    }
-  });
-});
-
-$(document).scroll(function() {
-  getAnimation();
-});
-
-$(document).ready(function() {
-
-  getAnimation();
-  getMeetUsSlider();
-  getPricesSlider();
-
-    if( $(".testimonial_slider").length > 0 ) {
-        $(".testimonial_slider").not(".slick-initialized").slick({
-            dots: false,
-            arrows: false,
-            // autoplay: true,
-            autoplaySpeed: 4000,
-            speed: 1200,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/arrow_red_right.svg"></button>',
-            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/arrow_red_right.svg"></button>',
-            responsive: [
-                {
-                  breakpoint: 1124,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                  }
-                },
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    variableWidth: true,
-                    arrows: true
-                  }
-                }
-              ]
-        });
-    }
-
-    if( $(".benefits_slider").length > 0 ) {
-      $(".benefits_slider").not(".slick-initialized").slick({
-          dots: true,
-          arrows: false,
-          // autoplay: true,
-          autoplaySpeed: 4000,
-          speed: 1200,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          fade: true,
-          appendDots: $(".benefits_slider_controls"),
-          appendArrows: $(".benefits_slider_arrows"),
-          responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  arrows: true
-                }
-              },
-              // {
-              //   breakpoint: 540,
-              //   settings: {
-              //     slidesToShow: 1,
-              //     slidesToScroll: 1
-              //   }
-              // }
-            ]
-      });
-    }
-
-    $( ".thumb_2" ).bind({
-      mouseenter: function() {
-        $( this ).find(".thumb_2_text").slideDown(300);
-      },
-      mouseleave: function() {
-        $( this ).find(".thumb_2_text").slideUp(300);
-      }
-    });
-
-    // -------------
-
-    $(".respmenubtn").on("click", function(e) {
-      e.preventDefault();
-      $("#respNav").toggleClass("visible");
-      $(".resp_bg").toggleClass("visible");
-    });
-
-    $(".resp_bg").on("click", function(e) {
-      e.preventDefault();
-      $("#respNav").removeClass("visible");
-      $(this).removeClass("visible");
-    });
-
-    $("#closeBtn").on("click", function(e) {
-      e.preventDefault();
-      $("#respNav").removeClass("visible");
-      $(".resp_bg").removeClass("visible");
-    });
-
-    $(this).keydown(function(eventObject){
-      if (eventObject.which == 27) {
-        if($("#respNav").hasClass("visible") && $(".resp_bg").hasClass("visible")) {
-          $("#respNav").removeClass("visible");
-          $(".resp_bg").removeClass("visible");
-        }
-      }
-    });
-
-    $(document).mouseup(function (e){
-        e.preventDefault();
-        var hide_element = $('#respNav');        
-        if (!hide_element.is(e.target)
-                && hide_element.has(e.target).length === 0) {
-                hide_element.removeClass("visible");
-                $(".resp_bg").removeClass("visible");
-        }
-    });
-
-    // --------
-
+function getPopupEvents() {
     $("[data-popup-link]").on("click", function(e) {
         e.preventDefault();
         popupName = $(this).attr("data-popup-link");
@@ -276,6 +143,170 @@ $(document).ready(function() {
             $("[data-popup]").fadeOut(300);
         }
       }
+    });
+}
+
+
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+  getRightCoord();
+});
+
+$(window).resize(function() {
+  getRightCoord();
+  bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+  getMeetUsSlider();
+  getPricesSlider();
+  // $( ".thumb_2" ).bind({
+  //   mouseenter: function() {
+  //     $( this ).find(".thumb_2_text").slideDown(300);
+  //   },
+  //   mouseleave: function() {
+  //     $( this ).find(".thumb_2_text").slideUp(300);
+  //   }
+  // });
+  getPopupEvents();
+});
+
+$(document).scroll(function() {
+  getAnimation();
+});
+
+$(document).ready(function() {
+
+  getAnimation();
+  getMeetUsSlider();
+  getPricesSlider();
+  getPopupEvents();
+
+    if( $(".testimonial_slider").length > 0 ) {
+        $(".testimonial_slider").not(".slick-initialized").slick({
+            dots: false,
+            arrows: false,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/arrow_red_right.svg"></button>',
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/arrow_red_right.svg"></button>',
+            responsive: [
+                {
+                  breakpoint: 1124,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                  }
+                },
+                {
+                  breakpoint: 900,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    variableWidth: true,
+                    arrows: true
+                  }
+                }
+              ]
+        });
+    }
+
+    if( $(".benefits_slider").length > 0 ) {
+
+      $(".benefits_slider_click").on("click", function(e) {
+        e.preventDefault();
+        parentBlock = $(this).closest(".benefits_slider_wrapp");
+        benefitsSliderNext = parentBlock.find(".slick-next").trigger("click");
+      });
+
+      $(".benefits_slider").not(".slick-initialized").slick({
+          dots: true,
+          arrows: true,
+          // autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true,
+          appendDots: $(".benefits_slider_controls"),
+          appendArrows: $(".benefits_slider_arrows"),
+          // responsive: [
+          //     {
+          //       breakpoint: 1024,
+          //       settings: {
+          //         arrows: true
+          //       }
+          //     },
+          //     // {
+          //     //   breakpoint: 540,
+          //     //   settings: {
+          //     //     slidesToShow: 1,
+          //     //     slidesToScroll: 1
+          //     //   }
+          //     // }
+          //   ]
+      });
+    }
+
+    // -------------
+
+    $(".respmenubtn").on("click", function(e) {
+      e.preventDefault();
+      $("#respNav").toggleClass("visible");
+      $(".resp_bg").toggleClass("visible");
+    });
+
+    $(".resp_bg").on("click", function(e) {
+      e.preventDefault();
+      $("#respNav").removeClass("visible");
+      $(this).removeClass("visible");
+    });
+
+    $("#closeBtn").on("click", function(e) {
+      e.preventDefault();
+      $("#respNav").removeClass("visible");
+      $(".resp_bg").removeClass("visible");
+    });
+
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+        if($("#respNav").hasClass("visible") && $(".resp_bg").hasClass("visible")) {
+          $("#respNav").removeClass("visible");
+          $(".resp_bg").removeClass("visible");
+        }
+      }
+    });
+
+    $(document).mouseup(function (e){
+        e.preventDefault();
+        var hide_element = $('#respNav');        
+        if (!hide_element.is(e.target)
+                && hide_element.has(e.target).length === 0) {
+                hide_element.removeClass("visible");
+                $(".resp_bg").removeClass("visible");
+        }
+    });
+
+    // -----------
+
+    $('.main_nav a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var hrefAttr = $(this).attr("href");
+        if( hrefAttr.length > 0 && hrefAttr != "#" ) {
+            $('html, body').stop().animate({
+                'scrollTop': $(hrefAttr).offset().top+2
+            }, 500);
+        }
+        if($("#respNav").hasClass("visible") && $(".resp_bg").hasClass("visible")) {
+          $("#respNav").removeClass("visible");
+          $(".resp_bg").removeClass("visible");
+        }
     });
 
 });
